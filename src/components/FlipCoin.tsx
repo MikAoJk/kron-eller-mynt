b9db7bc55ae65463ed425e82e0f2fee17a8cd9a1
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-05-08 09:16:09 +0200

    Update FlipCoin.tsx

--- src/components/FlipCoin.tsx
+++ src/components/FlipCoin.tsx
@@ -34,7 +34,7 @@ const FlipCoin = () => {
 
     return (
         <div className="flex flex-col items-center justify-center w-1/2 lg:w-1/3">
-            <h1 className="mb-6 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            <h1 className="mb-6 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
                 Kast 1-kronen</h1>
             {coinSideHead && !isEditing &&
                 <div>
