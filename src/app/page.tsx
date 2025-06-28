cae97d45a2b1389948ea89b5774034d8aaa7c186
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-06-28 21:09:04 +0200

    margin top 10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
     return (
         <main>
-            <div className="flex min-h-screen flex-col items-center justify-between md:p-12">
+            <div className="mt-10 flex min-h-screen flex-col items-center justify-between md:p-12">
                 <FlipCoin/>
             </div>
         </main>
