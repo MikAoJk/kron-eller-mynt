8a35d6f5ad808b92371be96754d6eadc897eb2af
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-06-28 20:58:38 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
 
     return (
         <main>
-            <div className="flex flex-col items-center justify-between p-10"></div>
             <div className="flex min-h-screen flex-col items-center justify-between md:p-12">
                 <FlipCoin/>
             </div>
